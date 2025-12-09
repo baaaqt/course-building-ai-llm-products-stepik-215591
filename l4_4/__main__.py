7a8235e0454af89688af26ebb3246cb167d00755
Bakyt Niiazaliev <bakyt.worker@gmail.com>
2025-12-09 20:25:43 +0600

    refactor: use parallel runnables to calculate two roots solution

--- l4_4/__main__.py
+++ l4_4/__main__.py
@@ -2,7 +2,9 @@ import math
 from pprint import pprint
 from typing import TypedDict, NotRequired
 from langchain_core.runnables import (
+    Runnable,
     RunnableLambda,
+    RunnableParallel,
     RunnablePassthrough,
     RunnableSequence,
 )
@@ -29,17 +31,21 @@ discriminant_runnable = RunnablePassthrough.assign(
 
 one_root_solution = RunnableLambda(lambda nums: -nums["b"] / (2 * nums["a"]))
 
-two_roots_solution = RunnableLambda(
-    lambda nums: (
-        (-nums["b"] + math.sqrt(nums["discriminant"])) / (2 * nums["a"]),
-        (-nums["b"] - math.sqrt(nums["discriminant"])) / (2 * nums["a"]),
-    )
+
+two_roots_x1 = RunnableLambda(
+    lambda nums: (-nums["b"] + math.sqrt(nums["discriminant"])) / (2 * nums["a"])
 )
+
+two_roots_x2 = RunnableLambda(
+    lambda nums: (-nums["b"] - math.sqrt(nums["discriminant"])) / (2 * nums["a"])
+)
+
+two_roots_solution = RunnableParallel(x1=two_roots_x1, x2=two_roots_x2)
 
 
 def roots_runable(
     nums: QuadraticEquationDataSecond,
-) -> RunnableLambda | None:
+) -> Runnable | None:
     if nums["discriminant"] > 0.0:
         return two_roots_solution
     elif nums["discriminant"] == 0.0:
@@ -53,8 +59,12 @@ roots = RunnableLambda(roots_runable)
 pipeline = RunnableSequence(discriminant_runnable, roots)
 
 pprint(pipeline.get_graph().draw_ascii())
+print("\n\n")
 
-assert set(pipeline.invoke({"a": 1, "b": -5, "c": 6})).issuperset({2.0, 3.0})
+result = pipeline.invoke({"a": 1, "b": -5, "c": 6})
+assert set((result["x1"], result["x2"])) == {2.0, 3.0}
 assert pipeline.invoke({"a": 1, "b": 2, "c": 1}) == -1.0
 assert pipeline.invoke({"a": 1, "b": 0, "c": 0}) == 0.0
 assert pipeline.invoke({"a": 1, "b": 0, "c": 1}) is None
+
+print("All tests passed")
