b46d7a3e4633374d9e680a5229cfe17d32b9ad86
Bakyt Niiazaliev <bakyt.worker@gmail.com>
2025-12-09 20:18:11 +0600

    Add quadratic equation solver with runnable pipeline

---
+++ l4_4/__main__.py
@@ -0,0 +1,60 @@
+import math
+from pprint import pprint
+from typing import TypedDict, NotRequired
+from langchain_core.runnables import (
+    RunnableLambda,
+    RunnablePassthrough,
+    RunnableSequence,
+)
+
+
+class QuadraticEquationDataFirst(TypedDict):
+    a: float
+    b: float
+    c: float
+    discriminant: NotRequired[float]
+
+
+class QuadraticEquationDataSecond(TypedDict):
+    a: float
+    b: float
+    c: float
+    discriminant: float
+
+
+discriminant_runnable = RunnablePassthrough.assign(
+    discriminant=RunnableLambda(lambda data: data["b"] ** 2 - 4 * data["a"] * data["c"])
+)
+
+
+one_root_solution = RunnableLambda(lambda nums: -nums["b"] / (2 * nums["a"]))
+
+two_roots_solution = RunnableLambda(
+    lambda nums: (
+        (-nums["b"] + math.sqrt(nums["discriminant"])) / (2 * nums["a"]),
+        (-nums["b"] - math.sqrt(nums["discriminant"])) / (2 * nums["a"]),
+    )
+)
+
+
+def roots_runable(
+    nums: QuadraticEquationDataSecond,
+) -> RunnableLambda | None:
+    if nums["discriminant"] > 0.0:
+        return two_roots_solution
+    elif nums["discriminant"] == 0.0:
+        return one_root_solution
+    return None
+
+
+roots = RunnableLambda(roots_runable)
+
+
+pipeline = RunnableSequence(discriminant_runnable, roots)
+
+pprint(pipeline.get_graph().draw_ascii())
+
+assert set(pipeline.invoke({"a": 1, "b": -5, "c": 6})).issuperset({2.0, 3.0})
+assert pipeline.invoke({"a": 1, "b": 2, "c": 1}) == -1.0
+assert pipeline.invoke({"a": 1, "b": 0, "c": 0}) == 0.0
+assert pipeline.invoke({"a": 1, "b": 0, "c": 1}) is None
